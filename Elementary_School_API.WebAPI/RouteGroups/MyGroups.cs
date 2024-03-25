247d18d121db1f2a5fdfda194b39118cc4fa12f8
Mohammad-Amn2000 <151476852+mohammadAmn-1998@users.noreply.github.com>
2024-03-25 20:59:45 +0330

    create userEndpoints Class

--- Elementary_School_API.WebAPI/RouteGroups/MyGroups.cs
+++ Elementary_School_API.WebAPI/RouteGroups/MyGroups.cs
@@ -67,54 +67,13 @@ namespace Elementary_School_API.WebAPI.RouteGroups
 		public static RouteGroupBuilder GroupUsers(this RouteGroupBuilder group)
 		{
 			//Register
-			group.MapPost("/", async ([FromForm]User user, IUserService service) =>
-			{
-
-				if (string.IsNullOrEmpty(user.UserName) || string.IsNullOrEmpty(user.Password))
-					return Results.ValidationProblem(new Dictionary<string, string[]>(),
-						detail: "userName or Password must be not empty!", statusCode: (int)HttpStatusCode.BadRequest);
-
-				var userNameAlreadyExists = await service.CheckUserNameExists(user.UserName!);
-
-				if (userNameAlreadyExists)
-					return Results.Problem(detail: "userName already exists!",
-						statusCode: StatusCodes.Status409Conflict);
-
-				var userId = await service.CreateUserAsync(new UserDto
-				{
-					Id = 0,
-					UserName = user.UserName,
-					Password = user.Password,
-					CreationTime = DateTime.Now,
-					UpdateTime = null,
-					IsDeleted = false
-				});
-
-				return userId > 0 ? Results.Created() : Results.StatusCode(StatusCodes.Status500InternalServerError);
-			}).DisableAntiforgery();
-
+			group.MapPost("/",UserEndpoints.RegisterAsync ).DisableAntiforgery();
 
 			//Login 
-			group.MapPost("/Login", async ([FromForm] LoginUserDto userDto,IUserService service,IConfiguration configuration) =>
-			{
-				if(string.IsNullOrEmpty(userDto.UserName) || string.IsNullOrEmpty(userDto.Password))
-					return Results.ValidationProblem(new Dictionary<string, string[]>(),
-						detail: "userName or Password must be not empty!", statusCode: (int)HttpStatusCode.BadRequest);
-
-
-				var currentUser = await service.GetUserByUserNameAndPassword(userDto.UserName!, userDto.Password!);
-				if (currentUser == null) return Results.NotFound();
-
-				var jwt = JwtTokenGenerator.GenerateJwtIdentityToken(configuration, currentUser.UserName!);
-
-				return jwt == null ? Results.StatusCode(StatusCodes.Status500InternalServerError) : Results.Ok(jwt);
-
-			}).DisableAntiforgery();
+			group.MapPost("/Login",UserEndpoints.LoginAsync).DisableAntiforgery();
 
 			return group;
 		}
-
-
 
 	}
 }
